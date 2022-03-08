42ce281e6678da367df6021cf3a4ea831f2ae112
Bruno Lehnen Simões <bruno.lehnen@gmail.com>
2022-03-08 20:18:14 +0100

    add list dropdown and title prompt to note plugin

--- lua/custom_plugins/notes.lua
+++ lua/custom_plugins/notes.lua
@@ -1,10 +1,46 @@
-function NewNote()
+local N = {}
+local utils = require('utils')
+local themes = require('telescope.themes')
+local a = require "plenary.async"
+
+ function OLDwriteToFile(path, text)
+    local err, fd = a.uv.fs_open(path, "w+", 438)
+    assert(not err, err)
+
+    err, fd = a.uv.fs_write(fd, text)
+    assert(not err, err)
+
+
+    err = a.uv.fs_close(fd)
+    assert(not err, err)
+end
+
+function N.NewNote()
     local timestamp = os.date("%d%m%y%H%M%S")
-    local filepath = string.format("~/notes/%s.md", timestamp)
+    local title = vim.fn.input({ prompt = 'Title of note: ' })
+    local filepath = string.format("~/notes/%s-%s.md", timestamp, string.gsub(title, ' ', '-'))
+    os.execute(string.format('echo "# %s" > %s', title, filepath))
+
+
     vim.cmd("vnew "..filepath)
 end
 
+function N.ListNotes()
+    local options = themes.get_dropdown {
+        winblend = 10,
+        border = true,
+        previewer = false,
+        shorten_path = false,
+        cwd = "~/notes"
+    }
 
-local utils = require('utils')
 
-utils.map('n', '<leader>tn', '<cmd>lua NewNote()<CR>')
+    require('telescope.builtin').find_files(options)
+end
+
+
+utils.map('n', '<leader>tn', '<cmd>lua require("custom_plugins.notes").NewNote()<CR>')
+utils.map('n', '<leader>fn', '<cmd>lua require("custom_plugins.notes").ListNotes()<CR>')
+
+
+return N
